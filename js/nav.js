7b8ab28eb434d5a4bb1d326e046b65cecb052896
Rupneet <rupneeta@sfu.ca>
2017-10-02 16:04:38 -0700

    Adding the documents to the main git folder - Style Guide start

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "30em";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
